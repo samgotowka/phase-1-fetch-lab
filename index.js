377599ccece0949be111879e4b208eb39b79c73b
Sam Gotowka <sam.gotowka@gmail.com>
2022-08-16 16:49:11 -0400

    commit tree 6c01fd8a2d11bcc28e139d6ee370f28d4cb3bd58

--- index.js
+++ index.js
@@ -15,3 +15,20 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+function fetchBooks(){
+  fetch("https://anapioficeandfire.com/api/books")
+  .then(function(resp) {
+    return resp.json
+  })
+  .then(function(json){
+    console.log(json)
+  })
+}
+function renderBooks(json){
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name} </h2>`
+    main.appendChild(h2)
+  })
+}
